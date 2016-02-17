5f2a17b784052800f5b0f7145991270d1262d6a8
nocturnal316 <nocturnal316@gmail.com>
2016-02-16 23:35:47 -0500

    Cleaned up dead files and reorganized everything
    Got rid of my run and read me since I will be producing a new readme
    with bashscript

--- Patricks_Files/drug_tweet_searcher.py
+++ Patricks_Files/drug_tweet_searcher.py
@@ -1,62 +1,65 @@
 import json
 import codecs
 
-class tweetSearch(object):
-      
-        @staticmethod
-        def searchDrugTweets():
-                tweets_file = open("1year_filtered.json")
-                tweet_keyword_file = open("Drug_Keywords.json")
 
-                k = json.load(tweet_keyword_file)
-                tweet_keyword_file.close()
-                keys = set(k["key_words"])
-                tags = set(k["hashtags"])
+def searchDrugTweets():
+		tweets_file = open("1year_filtered.json")
+		tweet_keyword_file = open("Drug_Keywords.json")
 
-                keys.union(tags)
+		k = json.load(tweet_keyword_file)
+		tweet_keyword_file.close()
+		keys = set(k["key_words"])
+		tags = set(k["hashtags"])
 
-                flagged_users = set()
+		keys.union(tags)
 
-                count = 0
-                file = open("Flagged_Drug_Tweets3.txt","w")
-                line = tweets_file.readline()
-                while line != "":
-                        try:
-                                j = json.loads(line)
-                                tweet = set(j["text"].encode('unicode-escape').replace('\\',' ').lower().split())
-                                temp = keys.intersection(tweet)
-                                if temp:
-                                        file.write(str(j["id"]))
-                                        file.write(";")
-                                        file.write(str(j["user"]["id_str"]))
-                                        file.write(";")
-                                        file.write(str(temp))
-                                        file.write(";")
-                                        file.write(str(j["created_at"])[5:25])
-                                        file.write(";")
-                                        file.write(str(j["text"].encode('unicode-escape')))
-                                        file.write("\n")
-                                        flagged_users.add(str(j["user"]["id_str"]))
-                                        count = count + 1	
-                        except ValueError:
-                                pass
-                                file.write("\n")
-                        line = tweets_file.readline()	
-                tweets_file.close()
-                file.close()
-                        
-                print "Number of Flagged Tweets:"	
-                print count
+		flagged_users = set()
 
-                count = 0
-                file = open("Flagged_Drug_Users1.txt","w")
-                for users in flagged_users:
-                        file.write(str(users))
-                        file.write("\n")
-                        count = count + 1
-                file.close()
+		count = 0
+		file = open("Flagged_Drug_Tweets3.txt","w")
+		line = tweets_file.readline()
+		while line != "":
+				try:
+						j = json.loads(line)
+						tweet = set(j["text"].encode('unicode-escape').replace('\\',' ').lower().split())
+						temp = keys.intersection(tweet)
+						if temp:
+								file.write(str(j["id"]))
+								file.write(";")
+								file.write(str(j["user"]["id_str"]))
+								file.write(";")
+								file.write(str(temp))
+								file.write(";")
+								file.write(str(j["created_at"])[5:25])
+								file.write(";")
+								file.write(str(j["text"].encode('unicode-escape')))
+								file.write("\n")
+								flagged_users.add(str(j["user"]["id_str"]))
+								count = count + 1	
+				except ValueError:
+						pass
+						file.write("\n")
+				line = tweets_file.readline()	
+		tweets_file.close()
+		file.close()
+				
+		print "Number of Flagged Tweets:"	
+		print count
 
-                print "Number of Flagged Users"
-                print count
+		count = 0
+		file = open("Flagged_Drug_Users1.txt","w")
+		for users in flagged_users:
+				file.write(str(users))
+				file.write("\n")
+				count = count + 1
+		file.close()
+
+		print "Number of Flagged Users"
+		print count
+				
+				
+if __name__ == "__main__":
+	searchDrugTweets()
+	
 
 
